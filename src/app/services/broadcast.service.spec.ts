ac0efee2b7fb69b5e7ebf6dc61e1548f3d11d7b5
sergey <herp7@i.ua>
2018-07-25 00:58:21 +0300

    add tests

--- src/app/services/broadcast.service.spec.ts
+++ src/app/services/broadcast.service.spec.ts
@@ -1,15 +1,20 @@
-import { TestBed, inject } from '@angular/core/testing';
+import {TestBed, inject} from '@angular/core/testing';
 
-import { BroadcastService } from './broadcast.service';
+import {BroadcastService} from './broadcast.service';
+import {EventEmitter} from '@angular/core';
 
 describe('BroadcastService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [BroadcastService]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [BroadcastService]
+        });
     });
-  });
 
-  it('should be created', inject([BroadcastService], (service: BroadcastService) => {
-    expect(service).toBeTruthy();
-  }));
+    it('Broadcast service should be created', inject([BroadcastService], (service: BroadcastService) => {
+        expect(service).toBeTruthy();
+    }));
+
+    it('Broadcast check subscriberToSearch', inject([BroadcastService], (service: BroadcastService) => {
+        expect(service.subscriberToSearch()).toEqual(new EventEmitter());
+    }));
 });
--- src/app/services/http.service.spec.ts
+++ src/app/services/http.service.spec.ts
@@ -1,15 +1,80 @@
-import { TestBed, inject } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
-import { HttpService } from './http.service';
+import {HttpService} from './http.service';
+import {Client} from './client';
 
 describe('HttpService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [HttpService]
-    });
-  });
+    let service: HttpService;
+    let httpMock: HttpTestingController;
 
-  it('should be created', inject([HttpService], (service: HttpService) => {
-    expect(service).toBeTruthy();
-  }));
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [HttpService]
+        });
+
+        service = TestBed.get(HttpService);
+        httpMock = TestBed.get(HttpTestingController);
+    });
+
+    afterEach(() => {
+        httpMock.verify();
+    });
+
+    it('Should retrieve from the API via GET', () => {
+        const dummyPost: Client[] = [
+            {
+                'general': {
+                    'firstName': 'Liana',
+                    'lastName': 'Crooks',
+                    'avatar': 'https://s3.amazonaws.com/uifaces/faces/twitter/kevinoh/128.jpg'
+                },
+                'job': {
+                    'company': 'Ledner, Johnson and Predovic',
+                    'title': 'Investor Functionality Coordinator'
+                },
+                'contact': {
+                    'email': 'Gerry_Hackett77@gmail.com',
+                    'phone': '(895) 984-0132'
+                },
+                'address': {
+                    'street': '1520 Zemlak Cove',
+                    'city': 'New Devon',
+                    'zipCode': '42586-7898',
+                    'country': 'Guinea-Bissau'
+                }
+            },
+            {
+                'general': {
+                    'firstName': 'Deontae',
+                    'lastName': 'Dare',
+                    'avatar': 'https://s3.amazonaws.com/uifaces/faces/twitter/andysolomon/128.jpg'
+                },
+                'job': {
+                    'company': 'DAmore, Dicki and Borer',
+                    'title': 'International Applications Consultant'
+                },
+                'contact': {
+                    'email': 'Kellie.Marvin38@yahoo.com',
+                    'phone': '1-615-843-3426 x600'
+                },
+                'address': {
+                    'street': '65901 Glover Terrace',
+                    'city': 'Alden ton',
+                    'zipCode': '57744-4248',
+                    'country': 'Kenya'
+                }
+            }
+        ];
+        service.getClients().subscribe(clients => {
+            expect(clients.length).toBe(2);
+            expect(clients).toEqual(dummyPost);
+        });
+
+        const request = httpMock.expectOne(`${service.url}`);
+        expect(request.request.method).toBe('GET');
+
+        request.flush(dummyPost);
+    });
 });
